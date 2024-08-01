a385f6a08b7fd903435b81684a79ef768841792c
Robin Lightfoot <robin.yonge@tractable.ai>
2024-08-01 10:18:20 +0100

    feat: remove sequencer (#16)

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,6 @@
 import { start, getDestination, Transport } from "tone";
 import { updateInputMessageLog } from "./logging";
 import { Channel, ControlChannel, allChannels } from "./channels";
-import { makeSequencer } from "./sequencer";
 import { WebSocketPort, timeTag } from "osc";
 import { messageStringToMessage } from "./utils";
 import { startWaveformLoop } from "./waveform";
@@ -29,36 +28,6 @@ oscPort.on("message", (oscMsg) => {
 //setup channels
 allChannels.initialise();
 
-//setup sequencer
-const grid = makeSequencer();
-var beat = 0;
-
-const startSequencerLoop = () => {
-  const repeat = (time) => {
-    const messages = [];
-    grid.forEach((row, index) => {
-      let item = row[beat];
-      let prevBeat = beat === 0 ? 7 : beat - 1;
-      document.getElementById(`${index}${prevBeat}`).style.textDecoration =
-        "none";
-      document.getElementById(`${index}${beat}`).style.textDecoration =
-        "underline";
-      if (item.isActive) {
-        const message = messageStringToMessage(
-          document.getElementById(`sequencer-message-field-${index}`).value,
-        );
-        messages.push(message);
-      }
-    });
-    if (messages.length > 0) {
-      oscPort.send({ packets: messages, timeTag: timeTag(time) });
-    }
-    beat = (beat + 1) % 8;
-  };
-
-  Transport.scheduleRepeat(repeat, "8n");
-};
-
 // setup event listeners/initialisers
 window.onload = () => {
   document
@@ -69,7 +38,6 @@ window.onload = () => {
       oscPort.open();
       console.log("OSC WebSocketPort opened");
       getDestination().volume.rampTo(-10, 0.001);
-      startSequencerLoop();
       startWaveformLoop();
       Transport.start();
     });
